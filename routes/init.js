6eb4c572514b255b2b37b2457e20423b0f3a482e
wtyysd1-boop <wtyysd1@gmail.com>
2026-08-04 09:00:45 +0800

    Add files via upload

--- routes/init.js
+++ routes/init.js
@@ -1,97 +1,169 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-
-const Area = require("../models/Area");
-
-
-router.get("/", async (req, res) => {
-
-    try {
-
-        const areas = [
-            {
-                id: "430100",
-                name: "长沙市"
-            },
-            {
-                id: "430200",
-                name: "株洲市"
-            },
-            {
-                id: "430300",
-                name: "湘潭市"
-            },
-            {
-                id: "430400",
-                name: "衡阳市"
-            },
-            {
-                id: "430500",
-                name: "邵阳市"
-            },
-            {
-                id: "430600",
-                name: "岳阳市"
-            },
-            {
-                id: "430700",
-                name: "常德市"
-            },
-            {
-                id: "430800",
-                name: "张家界市"
-            },
-            {
-                id: "430900",
-                name: "益阳市"
-            },
-            {
-                id: "431000",
-                name: "郴州市"
-            },
-            {
-                id: "431100",
-                name: "永州市"
-            },
-            {
-                id: "431200",
-                name: "怀化市"
-            },
-            {
-                id: "431300",
-                name: "娄底市"
-            },
-            {
-                id: "433100",
-                name: "湘西土家族苗族自治州"
-            }
-        ];
+const Area = require('../models/Area');
 
 
-        // 防止重复写入
-        await Area.deleteMany({});
-
-
-        await Area.insertMany(areas);
-
-
-        res.json({
-            code:0,
-            message:"Area初始化成功",
-            count:areas.length
-        });
-
-
-    } catch(error){
-
-        console.error(error);
-
-        res.status(500).json({
-            code:500,
-            message:error.message
-        });
-
+const HUNAN_AREAS = [
+  {
+    name: "长沙市",
+    adcode: 430100,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [112.938814, 28.228209]
     }
+  },
+  {
+    name: "株洲市",
+    adcode: 430200,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [113.134002, 27.827433]
+    }
+  },
+  {
+    name: "湘潭市",
+    adcode: 430300,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [112.944052, 27.82973]
+    }
+  },
+  {
+    name: "衡阳市",
+    adcode: 430400,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [112.571997, 26.89323]
+    }
+  },
+  {
+    name: "邵阳市",
+    adcode: 430500,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [111.467791, 27.238892]
+    }
+  },
+  {
+    name: "岳阳市",
+    adcode: 430600,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [113.132855, 29.37029]
+    }
+  },
+  {
+    name: "常德市",
+    adcode: 430700,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [111.698497, 29.031673]
+    }
+  },
+  {
+    name: "张家界市",
+    adcode: 430800,
+    level: "city",
+    center: {
+      type: "Point",
+      coordinates: [110.479191, 29.117096]
+    }
+  },
+  {
+    name: "益阳市",
+    adcode: 430900,
+    level: "city",
+    center:{
+      type:"Point",
+      coordinates:[112.35518,28.55386]
+    }
+  },
+  {
+    name:"郴州市",
+    adcode:431000,
+    level:"city",
+    center:{
+      type:"Point",
+      coordinates:[113.014717,25.77051]
+    }
+  },
+  {
+    name:"永州市",
+    adcode:431100,
+    level:"city",
+    center:{
+      type:"Point",
+      coordinates:[111.612146,26.420394]
+    }
+  },
+  {
+    name:"怀化市",
+    adcode:431200,
+    level:"city",
+    center:{
+      type:"Point",
+      coordinates:[109.97824,27.550082]
+    }
+  },
+  {
+    name:"娄底市",
+    adcode:431300,
+    level:"city",
+    center:{
+      type:"Point",
+      coordinates:[111.994499,27.699838]
+    }
+  },
+  {
+    name:"湘西州",
+    adcode:433100,
+    level:"city",
+    center:{
+      type:"Point",
+      coordinates:[109.739735,28.314296]
+    }
+  }
+];
+
+
+router.get("/", async(req,res,next)=>{
+
+try{
+
+for(const area of HUNAN_AREAS){
+
+ await Area.updateOne(
+   {
+    adcode:area.adcode
+   },
+   area,
+   {
+    upsert:true
+   }
+ );
+
+}
+
+
+res.json({
+ code:0,
+ message:"湖南区域初始化完成",
+ count:HUNAN_AREAS.length
+});
+
+
+}catch(err){
+
+ next(err);
+
+}
 
 });
 
